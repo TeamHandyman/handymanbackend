e7168f6771538b368fe1ade9a492177d6217618f
Hirunika Gunathunga <84180743+HirunikaGunathunga@users.noreply.github.com>
2022-08-14 10:21:10 +0530

    Initialized admin login backend

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/test')
+
+
+
+app.post('/api/login', async (req, res) => {
+	console.log(req.body)
+	const user = await User.findOne({
+		email: req.body.email,
+	})
+	console.log(okay);
+	if (!user) {
+		return { status: 'error', error: 'Invalid login' }
+	}
+
+	const isPasswordValid = await bcrypt.compare(
+		req.body.password,
+		user.password
+	)
+
+	if (isPasswordValid) {
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			},
+			'secret123'
+		)
+
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
+})
+
+
+
+
+app.listen(1337, () => {
+	console.log('Server started on 1337')
+})
